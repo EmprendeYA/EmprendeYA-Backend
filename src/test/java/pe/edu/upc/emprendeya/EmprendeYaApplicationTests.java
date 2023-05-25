5325ea0ddcd2574c8cafc794a83def919d017cb4
unknown <jennyespinoza618@gmail.com>
2023-05-24 19:44:40 -0500

    first commit

---
+++ src/test/java/pe/edu/upc/emprendeya/EmprendeYaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.emprendeya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprendeYaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
