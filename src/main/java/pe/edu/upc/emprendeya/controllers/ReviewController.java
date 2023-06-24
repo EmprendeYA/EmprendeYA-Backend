12aa699c53639e2f3d68f3ae70bcd5a0f8d1edcd
marcoscarlos9 <marcoscarlosaviles@gmail.com>
2023-06-24 00:35:13 -0500

    final commit

--- src/main/java/pe/edu/upc/emprendeya/controllers/ReviewController.java
+++
@@ -1,4 +0,0 @@
-package pe.edu.upc.emprendeya.controllers;
-
-public class ReviewController {
-}
--- src/main/java/pe/edu/upc/emprendeya/repositories/IReviewRepository.java
+++
@@ -1,4 +0,0 @@
-package pe.edu.upc.emprendeya.repositories;
-
-public interface IReviewRepository {
-}
